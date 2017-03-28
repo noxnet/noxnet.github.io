3a23a2288057416db20e07816babe7ee74071100
Lumater <lumaterlord@gmail.com>
2017-03-28 15:54:15 -0700

    Edited.

--- YonnonBOX/users/login/users.js
+++ YonnonBOX/users/login/users.js
@@ -4,36 +4,15 @@ var login = function(username, password) {
             if (user == username) {
                   document.cookie = "user=" + user;
                   window.location.href = "noxnet.github.io/YonnonBOX/";
-                  location.reload();
+                  window.location.reload();
             } else {
                alert("INVALID LOGIN, OR LOGIN NOT REGISTERED");
                window.location.href = "noxnet.github.io/YonnonBOX/";
+               window.location.reload();
             }
 }
 var Validate = function() {
-            var user = document.forms["access_granted"]["username"].value;
-            var pass = document.forms["access_granted"]["password"].value;
-            var button = document.getElementById["submit"].value;
-            if (user === "public_non") {
-               if (pass === "vbdzxy") {
-                   document.cookie = "user=public_non";
-                   alert("ACCESSING PUBLIC USER");
-                   window.location.href = "noxnet.github.io/YonnonBOX/";
-                   window.location.reload();
-               }
-            }
-            if (user === "red_hand") {
-                 if (pass === "noxuser") {
-                     document.cookie = "user=RED_hand";
-                     alert("ACCESSING RED HAND");
-                     window.location.href = "noxnet.github.io/YonnonBOX/";
-                     window.location.reload();
-                 }
-            } else {
-              document.cookie = "user=NOSIGN";
-              alert("LOGIN UNSUCCESSFUL");
-              window.location.href = "noxnet.github.io/YonnonBOX/";
-              window.location.reload();
-            }
-        }
+            login("public_non", "vbdxzy");
+            login("red_hand", "gortzonx");
+}
 
