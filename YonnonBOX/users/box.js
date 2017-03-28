d50c162160d816108ea8ff62fd953e060b93a784
Lumater <lumaterlord@gmail.com>
2017-03-28 13:49:52 -0400

    Edited.

---
+++ YonnonBOX/users/box.js
@@ -0,0 +1,9 @@
+var GetUser = function() {
+   var user = document.cookie;
+   if (user == "") {
+      
+   } else {
+      var box = document.getElementById["user2"];
+      box.document.write("<h3>" + user + "</h3>");
+   }
+}
