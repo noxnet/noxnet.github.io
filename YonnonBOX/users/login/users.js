58291c0d1b792315d5ee01ed972f3fab8996f9f2
Lumater <lumaterlord@gmail.com>
2017-03-28 12:36:42 -0400

    Edited.

--- YonnonBOX/users/login/users.js
+++ YonnonBOX/users/login/users.js
@@ -4,19 +4,18 @@ var Validate = function() {
             if (user === "public_non") {
                if (pass === "vbdzxy") {
                    document.cookie = "user=public_non";
+                   alert("ACCESSING PUBLIC USER");
                }
             }
             if (user === "red_hand") {
                  if (pass === "noxuser") {
                      document.cookie = "user=RED_hand";
+                     alert("ACCESSING RED HAND");
                  }
             } else {
               document.cookie = "user=NOSIGN";
+              alert("LOGIN UNSUCCESSFUL");
+              window.location.href = "YonnonBOX";
             }
         }
 
-var VALIDATE = function() {
-   if (document.cookie === "ZVDX=USERACTIVE") {
-       Validate();
-   }
-}
