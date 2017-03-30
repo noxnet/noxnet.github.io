f6429371422e3c051451a26c4e5ea48beb0f5d7f
Lumater <lumaterlord@gmail.com>
2017-03-30 14:31:04 -0400

    Edited.

--- YonnonBOX/users/login/users.js
+++ YonnonBOX/users/login/users.js
@@ -1,15 +1,3 @@
-var login = function() {
-            var user = document.forms["access_granted"]["username"].value;
-            var pass = document.forms["access_granted"]["password"].value;
-            if (pass == "vbdxj6578$mN") {
-                var usr = "user=" + user;
-                alert(usr);
-                document.cookie = usr;
-                
-            } else {
-               alert("LOGIN FAILED");
-            }
-}
 var Validate = function() {
        var user = document.forms["access_granted"]["username"].value;
        var pass = document.forms["access_granted"]["password"].value;
