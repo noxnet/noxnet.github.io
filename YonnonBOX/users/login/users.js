20358eed5a65b0057e9d1253a6a56738d17a5720
Lumater <lumaterlord@gmail.com>
2017-03-30 12:12:02 -0400

    Edited.

--- YonnonBOX/users/login/users.js
+++ YonnonBOX/users/login/users.js
@@ -1,20 +1,12 @@
-var checkVar = function(vari) {
-            if (vari == false) {
-                 return true;           
-            } else {
-               return false;           
-            }
-}
 var login = function() {
             var user = document.forms["access_granted"]["username"].value;
             var pass = document.forms["access_granted"]["password"].value;
             if (pass == "vbdxj6578$mN") {
                 var usr = "user=" + user;
                 document.cookie = usr;
-                window.location.href = "noxnet.github.io/YonnonBOX/users/login/right.html";
                 
             } else {
-               window.location.href = "noxnet.github.io/YonnonBOX/users/login/wrong.html";
+               alert("LOGIN FAILED");
             }
 }
 var Validate = function() {
