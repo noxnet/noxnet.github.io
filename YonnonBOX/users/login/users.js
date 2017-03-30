fa2656010c7376b81c7c11196a647c6c55c6af58
Lumater <lumaterlord@gmail.com>
2017-03-30 15:46:02 -0700

    Edited.

--- YonnonBOX/users/login/users.js
+++ YonnonBOX/users/login/users.js
@@ -12,6 +12,6 @@ var Validate = function() {
        alert(pass);*/
 }
 var KILL = function() {
-      document.cookie "user=; expires=Thu, 01 Jan 1970 00:00:01 GMT;";
+      document.cookie = "user=; expires=Thu, 01 Jan 1970 00:00:01 GMT;";
 }
 
